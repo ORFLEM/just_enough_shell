a44ce13d9fcccb0a74debb35156ca5deacc53df1
_ORFLEM_ <zenkinzahar@gmail.com>
2025-11-05 19:03:02 +0700

    5.11.2025 update: adding chaotic repository
    Former-commit-id: 1a3f2ee4c65de6763f2592da41550ac1a8423683

--- flake.nix
+++ flake.nix
@@ -3,23 +3,24 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nur.url = "github:nix-community/NUR";  # ← Добавляем NUR
+    nur.url = "github:nix-community/NUR";
+    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
   };
   
-  outputs = { self, nixpkgs, nixpkgs-unstable, nur }: let
+  outputs = { self, nixpkgs, nixpkgs-unstable, nur, chaotic, ... }: let
     system = "x86_64-linux";
   in {
     nixosConfigurations.ORFLEMPC = nixpkgs.lib.nixosSystem {
       inherit system;
       modules = [
         ./configuration.nix
-        ({ config, pkgs, lib, ... }: {
+        chaotic.nixosModules.default  # Добавляем Chaotic
+        ({ config, pkgs, ... }: {
           _module.args.unstablePkgs = import nixpkgs-unstable { 
             inherit system; 
             config.allowUnfree = true;
           };
           
-          # Подключаем NUR
           nixpkgs.overlays = [
             nur.overlays.default
           ];
@@ -28,18 +29,6 @@
             package = config._module.args.unstablePkgs.hyprland;
             portalPackage = config._module.args.unstablePkgs.xdg-desktop-portal-hyprland;
           };
-          
-          environment.systemPackages = (with config._module.args.unstablePkgs; [
-            telegram-desktop
-            hyprland
-            hyprlandPlugins.hy3
-            hyprlandPlugins.hyprspace
-            hyprlandPlugins.hypr-dynamic-cursors
-          ]) ++ (with pkgs.nur.repos; [
-            # Примеры пакетов из NUR:
-            # rycee.firefox-addons.ublock-origin
-            # или любой другой репо из NUR
-          ]);
         })
       ];
     };
